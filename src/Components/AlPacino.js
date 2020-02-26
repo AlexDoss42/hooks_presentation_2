4b490939edfa85a1595b64be42ca89b2b5bd2874
AlexDoss42 <alex.doss42@gmail.com>
2020-02-26 12:00:48 -0700

    adds Al Pacino component

--- src/Components/AlPacino.js
+++ src/Components/AlPacino.js
@@ -14,14 +14,14 @@ const useStyles = makeStyles({
     }
 });
 
-const MarlonBrando = () => {
+const AlPacino = () => {
     const classes = useStyles();
 
     return (
         <Card classes={{ root: classes.card }}>
-            <p>So you didn't need a friend like me. Now you come and say "Don Corleone, give me justice." But you don't ask with respect. You don't offer friendship. You don't even think to call me "Godfather." You come into my house on the day my daughter is to be married and you ask me to do murder - for money.</p>
+            <p>My father taught me many things here. He taught me in this room. He taught me; "Keep your friends close, but your enemies closer.</p>
         </Card>
     )
 };
 
-export default MarlonBrando;
+export default AlPacino;
